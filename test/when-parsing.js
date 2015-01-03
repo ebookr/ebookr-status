ccf3abe5f2e3576b88e867015f15e6e2bd92643a
Arne Hassel <arne.hassel@gmail.com>
2015-01-03 19:13:42 +0100

    Added support for verbose mode

--- test/when-parsing.js
+++ test/when-parsing.js
@@ -1,11 +1,21 @@
-var expect = require('chai').expect;
+var chai = require('chai'),
+		sinonChai = require('sinon-chai'),
+		expect = chai.expect,
+		mockrequire = require('mockrequire'),
+		sinon = require('sinon');
+
+chai.use(sinonChai);
 
 describe('When parsing', function () {
-	var ebookr;
+	var ebookr, console;
 
 	beforeEach(function () {
 		ebookr = require('ebookr').new();
-		require('../lib/status')(ebookr);
+		console = { info: sinon.spy() };
+		mockrequire('../lib/status', {
+			'consoleplusplus': console,
+			'util': require('util')
+		})(ebookr);
 	});
 
 	describe('With metadata given', function () {
@@ -28,6 +38,13 @@ describe('When parsing', function () {
 			expect(function () {
 				ebookr.parse('<status state="2" />');
 			}).to.throw(/Invalid state given: 2/)
+		});
+
+		it('should support verbose mode', function () {
+			console.log
+			ebookr.option.set('verbose', true);
+			ebookr.parse('<status state="1" />');
+			expect(console.info).to.have.been.calledWith('Parsing tag: status { state: 1 }');
 		});
 	});
 
--- test/when-rendering.js
+++ test/when-rendering.js
@@ -1,11 +1,21 @@
-var expect = require('chai').expect;
+var chai = require('chai'),
+		sinonChai = require('sinon-chai'),
+		expect = chai.expect,
+		mockrequire = require('mockrequire'),
+		sinon = require('sinon');
+
+chai.use(sinonChai);
 
 describe('When rendering', function () {
-	var ebookr;
+	var ebookr, console;
 
 	beforeEach(function () {
 		ebookr = require('ebookr').new();
-		require('../lib/status')(ebookr);
+		console = { info: sinon.spy() };
+		mockrequire('../lib/status', {
+			'consoleplusplus': console,
+			'util': require('util')
+		})(ebookr);
 		ebookr.metadata.set('status', {
 			1: 'foo',
 			2: 'bar'
@@ -23,5 +33,11 @@ describe('When rendering', function () {
 			<status state="2" />').render()).to.equal('Status: foo (1)\
 			Status: foo (1)\
 			Status: bar (2)');
+	});
+
+	it('should support verbose mode', function () {
+		ebookr.option.set('verbose', true);
+		ebookr.parse('<status state="1" />').render();
+		expect(console.info).to.have.been.calledWith('Rendering tag: status { state: 1, label: foo, url: undefined }');
 	});
 });
